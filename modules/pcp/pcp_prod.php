d163d1dc6dc73282c95bafe8bf6bd4df2a12c516
Lucas Miguel Alves Lourenço <160145726+luccas94@users.noreply.github.com>
2025-07-17 11:54:05 -0300

    Add files via upload

--- modules/pcp/pcp_prod.php
+++ modules/pcp/pcp_prod.php
@@ -7,6 +7,7 @@ require_once __DIR__ . '/projecao_cervejas_functions.php';
 // Lista das cervejas que devem ser exibidas
 $cervejas_permitidas = [
     'WELT PILSEN',
+    "JUICY JILL",
     'DOG SAVE THE BEER', 
     'ZE DO MORRO',
     'HECTOR 5 ROUNDS',
