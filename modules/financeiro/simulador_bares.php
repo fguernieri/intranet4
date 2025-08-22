d5a0351d53b80981270a541778b8462aa41db96e
Lucas Miguel Alves Lourenço <160145726+luccas94@users.noreply.github.com>
2025-08-22 16:52:20 -0300

    Add files via upload

--- modules/financeiro/simulador_bares.php
+++ modules/financeiro/simulador_bares.php
@@ -6,6 +6,9 @@ ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
 require_once dirname(__FILE__) . '/db_config_financeiro.php';
+if (!defined('SUPABASE_URL')) {
+    die('SUPABASE_URL não definida. Verifique o arquivo db_config_financeiro.php');
+}
 
 // Função genérica para requisições REST Supabase
 function supabase_request($table, $method = 'GET', $params = '', $body = null) {
